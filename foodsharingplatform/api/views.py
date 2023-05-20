3cbcad9e2af91c0e6e89117c02e6dedd9e8a5a0d
THARAGESHWARAN-SATHYAMOORTHY <s.tharageshwaran6343000@gmail.com>
2023-05-20 17:22:33 +0530

    small corrections

--- foodsharingplatform/api/views.py
+++ foodsharingplatform/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, FoodItem, Donation
+from .serializers import UserSerializer, FoodItemSerializer, DonationSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class FoodItemViewSet(viewsets.ModelViewSet):
+    queryset = FoodItem.objects.all()
+    serializer_class = FoodItemSerializer
+
+class DonationViewSet(viewsets.ModelViewSet):
+    queryset = Donation.objects.all()
+    serializer_class = DonationSerializer
--- foodsharingplatform/frontend/src/App.js
+++ foodsharingplatform/frontend/src/App.js
@@ -1,24 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to the Food Sharing Platform!</p>
+    </div>
+  );
+}
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/users/')
+      .then((response) => setUsers(response.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+      <Link to="/users/create">Create User</Link>
+    </div>
+  );
+}
+
+function CreateUser() {
+  return (
+    <div>
+      <h2>Create User</h2>
+      <p>Form to create a new user goes here</p>
+    </div>
+  );
+}
+
+function FoodItems() {
+  const [foodItems, setFoodItems] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/fooditems/')
+      .then((response) => setFoodItems(response.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Food Items</h2>
+      <ul>
+        {foodItems.map((foodItem) => (
+          <li key={foodItem.id}>
+            {foodItem.name} - {foodItem.description}
+          </li>
+        ))}
+      </ul>
+      <Link to="/fooditems/create">Create Food Item</Link>
+    </div>
+  );
+}
+
+function CreateFoodItem() {
+  return (
+    <div>
+      <h2>Create Food Item</h2>
+      <p>Form to create a new food item goes here</p>
+    </div>
+  );
+}
+
+function Donations() {
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/donations/')
+      .then((response) => setDonations(response.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Donations</h2>
+      <ul>
+        {donations.map((donation) => (
+          <li key={donation.id}>
+            {donation.donor.name} donated {donation.food_item.name} ({donation.quantity})
+          </li>
+        ))}
+      </ul>
+      <Link to="/donations/create">Create Donation</Link>
+    </div>
+  );
+}
+
+function CreateDonation() {
+  return (
+    <div>
+      <h2>Create Donation</h2>
+      <p>Form to create a new donation goes here</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1 className="header">Food Sharing Platform</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/fooditems">Food Items</Link>
+            </li>
+            <li>
+              <Link to="/donations">Donations</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/users" exact component={Users} />
+        <Route path="/users/create" component={CreateUser} />
+        <Route path="/fooditems" exact component={FoodItems} />
+        <Route path="/fooditems/create" component={CreateFoodItem} />
+        <Route path="/donations" exact component={Donations} />
+        <Route path="/donations/create" component={CreateDonation} />
+      </div>
+    </Router>
   );
 }
 
