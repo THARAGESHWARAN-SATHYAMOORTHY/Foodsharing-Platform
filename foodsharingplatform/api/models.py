3c7de7d56928c5ccedc83bd511beeb06ff79c113
THARAGESHWARAN-SATHYAMOORTHY <s.tharageshwaran6343000@gmail.com>
2023-05-20 17:12:02 +0530

    dependencies

--- foodsharingplatform/api/models.py
+++ foodsharingplatform/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Donation(models.Model):
+    donor = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Donation #{self.id}"
---
+++ foodsharingplatform/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import User, FoodItem, Donation
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class FoodItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FoodItem
+        fields = '__all__'
+
+class DonationSerializer(serializers.ModelSerializer):
+    donor = UserSerializer()
+    food_item = FoodItemSerializer()
+
+    class Meta:
+        model = Donation
+        fields = '__all__'
