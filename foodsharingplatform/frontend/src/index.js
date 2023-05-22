aaa84b78e572e3588a9c2767b747c6cbf5469d5a
THARAGESHWARAN-SATHYAMOORTHY <s.tharageshwaran6343000@gmail.com>
2023-05-22 07:54:26 +0530

    complete update

--- foodsharingplatform/frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.css';
-import './tailwind.css';
-
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-createRoot(document.getElementById('root')).render(<App />);
